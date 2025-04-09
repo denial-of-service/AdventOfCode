e23ed91abebe91e2cc296b9b014f682b7afd1620
denial-of-service <94308892+denial-of-service@users.noreply.github.com>
2025-04-09 20:33:11 +0200

    Added year 2023 day 2 solution

---
+++ src/year_2023/day_02.py
@@ -0,0 +1,49 @@
+import utils
+
+
+def part1() -> int:
+    puzzle_input: str = utils.get_puzzle_input(2023, 2)
+    cube_limits: dict[str, int] = {'red': 12, 'green': 13, 'blue': 14}
+    sum_of_game_ids: int = 0
+
+    for game in puzzle_input.splitlines():
+        game_id_section, sets_of_cubes = game.split(': ')
+        game_id: int = int(game_id_section.split(' ')[1])
+        is_possible: bool = True
+        for set_of_cubes in sets_of_cubes.split('; '):
+            for cubes in set_of_cubes.split(', '):
+                cubes_count_section, color = cubes.split(' ')
+                cubes_count: int = int(cubes_count_section)
+                if cubes_count > cube_limits[color]:
+                    is_possible = False
+                    break
+            if not is_possible:
+                break
+
+        if is_possible:
+            sum_of_game_ids += game_id
+
+    return sum_of_game_ids
+
+
+def part2() -> int:
+    puzzle_input: str = utils.get_puzzle_input(2023, 2)
+    total_power: int = 0
+
+    for game in puzzle_input.splitlines():
+        max_cubes: dict[str, int] = dict.fromkeys(['red', 'green', 'blue'], 0)
+        sets_of_cubes = game.split(': ')[1]
+        for set_of_cubes in sets_of_cubes.split('; '):
+            for cubes in set_of_cubes.split(', '):
+                cubes_count_section, color = cubes.split(' ')
+                cubes_count: int = int(cubes_count_section)
+                max_cubes[color] = max(max_cubes[color], cubes_count)
+
+        total_power += max_cubes['red'] * max_cubes['green'] * max_cubes['blue']
+
+    return total_power
+
+
+if __name__ == '__main__':
+    print(part1())
+    print(part2())
--- test/test_2023.py
+++ test/test_2023.py
@@ -1,12 +1,16 @@
 import unittest
 
-from year_2023 import day_01, day_04
+from year_2023 import day_01, day_02, day_04
 
 
 class Test2023(unittest.TestCase):
     def test_day_01(self):
         self.assertEqual(56397, day_01.part1())
         self.assertEqual(55701, day_01.part2())
+
+    def test_day_02(self):
+        self.assertEqual(2317, day_02.part1())
+        self.assertEqual(74804, day_02.part2())
 
     def test_day_04(self):
         self.assertEqual(27845, day_04.part1())
